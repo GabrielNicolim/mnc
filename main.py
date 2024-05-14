947602587cbba82b238610c5a459fe59bc5e8bd2
Gabriel Gomes Nicolim <gabriel.gomes.nicolim@gmail.com>
2024-05-13 22:55:29 -0300

    Decompositon LU

--- main.py
+++ main.py
@@ -39,3 +39,51 @@ def SistemaTriangularSuperior(order, matrix, vector):
         solution[i] /= matrix[i][i]
 
     return solution
+
+# Questão 04
+
+def DecomposicaoLU(order, matrix, vector):
+    L = [[0] * order for _ in range(order)]
+    U = [[0] * order for _ in range(order)]
+
+    for i in range(order):
+        for k in range(i, order):
+            sum = 0
+
+            for j in range(i):
+                sum += (L[i][j] * U[j][k])
+
+            U[i][k] = matrix[i][k] - sum
+
+        for k in range(i, order):
+            if i == k:
+                L[i][i] = 1
+            else:
+                sum = 0
+                for j in range(i):
+                    sum += (L[k][j] * U[j][i])
+
+                L[k][i] = (matrix[k][i] - sum) / U[i][i]
+
+    y = [0] * order
+    x = [0] * order
+
+    for i in range(order):
+        y[i] = vector[i]
+
+        for j in range(i):
+            y[i] -= L[i][j] * y[j]
+
+    for i in range(order - 1, -1, -1):
+        x[i] = y[i]
+
+        for j in range(i + 1, order):
+            x[i] -= U[i][j] * x[j]
+
+        x[i] /= U[i][i]
+
+    return x
+
+# Questão 05
+
+
